b8daef5e13ab119135c9de8949acfe85204227ec
itsahmed98 <ahmed.akram98@yahoo.ca>
2025-06-05 20:43:28 -0400

    Initialiser un projet dotnet mvc et un projet de test xunit

---
+++ MagasinCentral/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagasinCentral.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
